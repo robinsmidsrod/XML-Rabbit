a027552967dd80a02dbd3e84eb7d365685de2a78
Robin Smidsrød <robin@smidsrod.no>
2010-06-13 11:02:16 +0200

    Updated docs on DOM memory issues and default namespace prefix

--- lib/XML/Rabbit.pm
+++ lib/XML/Rabbit.pm
@@ -123,8 +123,10 @@ types, you need to specify C<isa_map> instead of just specifying the types
 as a union type constraint in C<isa>. If you specify C<isa_map> you should
 not specify C<isa> aswell, as it will be overridden by the trait. The trait
 will wrap the type constraint union in an ArrayRef if the trait name is
-XPathObjectList. As all the traits that end with List return array
-references, their C<isa> must be an ArrayRef.
+XPathObjectList and as a HashRef if the trait name is XPathObjectMap. As all
+the traits that end with List return array references, their C<isa> must be
+an ArrayRef. The same is valid for the *Map traits, just that they return
+HashRef instead of ArrayRef.
 
 The namespace prefix used in C<isa_map> MUST be specified in the
 C<namespace_map>. If a prefix is used in C<isa_map> without a corresponding
@@ -136,8 +138,17 @@ entry in C<namespace_map> an exception will be thrown.
 Be aware of the syntax of XPath when used with namespaces. You should almost
 always define C<namespace_map> when dealing with XML that use namespaces.
 Namespaces explicitly declared in the XML are usable with the prefix
-specified in the XML (except if you use C<isa_map>). See
+specified in the XML (except if you use C<isa_map>). Be aware that a prefix
+must usually be declared for the default namespace (xmlns=...) to be able to
+use it in XPath queries. See the example above (on XHTML) for details. See
 L<XML::LibXML::Node/findnodes> for more information.
+
+Because XML::Rabbit uses XML::LibXML's DOM parser it is limited to handling
+XML documents that can fit in available memory. Unfortunately there is no
+easy way around this, because XPath queries need to work on a tree model,
+and I am not aware of any way of doing that without keeping the document in
+memory. Luckily XML::LibXML's DOM implementation is written in C, so it
+should use much less memory than a pure Perl DOM parser.
 
 
 =head1 SEMANTIC VERSIONING
